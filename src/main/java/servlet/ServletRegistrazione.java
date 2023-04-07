b9d41e25fbed87e1d71f154f4803ac6cddba9e84
Apio <elia.marangon@aldini.istruzioneer.it>
2023-04-07 13:54:22 +0200

    Implementato Servlet_Accesso

--- src/main/java/servlet/ServletRegistrazione.java
+++ src/main/java/servlet/ServletRegistrazione.java
@@ -1,17 +1,65 @@
 package servlet;
 
 import java.io.*;
+import java.sql.SQLException;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import dao.UtenzeDao;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "registrazione", value = "/registrazione")
 public class ServletRegistrazione extends HttpServlet {
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-	}
-
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String body = getBody(request);
+		// CREDO UN JSON PER IL RISULTATO
+		JsonObject temp = new Gson().fromJson(body, JsonObject.class);
+		// fromJason => trasforma da stringa a Json, prende in input -stringa- -tipo destinazione-
+
+		String nome = temp.get("nome_utente").toString(); // TROVO IL NOME
+		String password = temp.get("password_utente").toString(); // TROVO LA PASSWORD
+
+		UtenzeDao DataAccessObject = null;
+		try {
+			DataAccessObject = new UtenzeDao(); // CREDO ISTANZA UTENZE_DAO
+			DataAccessObject.insertUtenza(nome, password); // CREO INSERISCO I DATI CHE VERRANNO MANDATI AL DB
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
 	}
 
+	// PRESA DA INTERNET, SI OCCUPA DI FARE IL BODY DELLA RICHIESTA
+	public static String getBody(HttpServletRequest request) throws IOException {
+		String body = null;
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader bufferedReader = null;
+		try {
+			InputStream inputStream = request.getInputStream();
+			if (inputStream != null) {
+				bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+				char[] charBuffer = new char[128];
+				int bytesRead = -1;
+				while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+					stringBuilder.append(charBuffer, 0, bytesRead);
+				}
+			} else {
+				stringBuilder.append("");
+			}
+		} catch (IOException ex) {
+			throw ex;
+		} finally {
+			if (bufferedReader != null) {
+				try {
+					bufferedReader.close();
+				} catch (IOException ex) {
+					throw ex;
+				}
+			}
+		}
+		body = stringBuilder.toString();
+		return body;
+	}
 }

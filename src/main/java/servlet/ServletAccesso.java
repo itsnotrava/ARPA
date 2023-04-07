4bd73b7f7e4ed8fda5e4c1cf039fbc8abe36b77e
Nicola Travaglini <nicola1.travaglini@gmail.com>
2023-04-07 13:13:28 +0200

    Aggiunta Servlets Sprint #1

---
+++ src/main/java/servlet/ServletAccesso.java
@@ -0,0 +1,17 @@
+package servlet;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "accesso", value = "/accesso")
+public class ServletAccesso extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	}
+
+}

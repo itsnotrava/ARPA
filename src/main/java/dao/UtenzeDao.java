ca19a66be7f98e04b0bf54f9c21f289980bf7a6e
Nicola Travaglini <nicola1.travaglini@gmail.com>
2023-04-07 11:02:17 +0200

    Implementazione CRUD (no Update) su UtenzeDao.java

--- src/main/java/dao/UtenzeDao.java
+++ src/main/java/dao/UtenzeDao.java
@@ -4,6 +4,7 @@ import factory.ConnectionFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UtenzeDao {
@@ -21,4 +22,43 @@ public class UtenzeDao {
 
 		preparedStatement.execute();
 	}
+
+	public boolean verifyUtenza(String email, String password) throws SQLException {
+		String sql = "SELECT id FROM Utenze WHERE email=? AND password=?";
+		PreparedStatement preparedStatement = this.connection.prepareStatement(sql);
+		preparedStatement.setString(1, email);
+		preparedStatement.setString(2, password);
+
+		ResultSet resultSet = preparedStatement.executeQuery();
+		resultSet.next();
+		return resultSet.getInt(1) != 0;
+	}
+
+	/**
+	 * Metodo che da email e password returna 0 se l'utente non esiste,
+	 * e il suo ID se invece esiste.
+	 * @param email
+	 * @param password
+	 * @return id [0, *]
+	 * @throws SQLException
+	 */
+	public int getIdFromUtenza(String email, String password) throws SQLException {
+		String sql = "SELECT id FROM Utenze WHERE email=? AND password=?";
+		PreparedStatement preparedStatement = this.connection.prepareStatement(sql);
+		preparedStatement.setString(1, email);
+		preparedStatement.setString(2, password);
+
+		ResultSet resultSet = preparedStatement.executeQuery();
+		resultSet.next();
+		return resultSet.getInt(1);
+	}
+
+	public void deleteUtenza(int id) throws SQLException {
+		String sql = "DELETE FROM Utenze WHERE id=?";
+		PreparedStatement preparedStatement = this.connection.prepareStatement(sql);
+		preparedStatement.setInt(1, id);
+
+		preparedStatement.execute();
+	}
+
 }
